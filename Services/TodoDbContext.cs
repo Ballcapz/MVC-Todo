40a7af4fb9aed1af12c2a481ff729ce0e3aa688d
Zach Johnson <zjohnson@omnitech-inc.com>
2020-06-10 08:00:23 -0500

    Refactored out to using a proper todorepository, rather than right in the dbcontext

--- Services/TodoDbContext.cs
+++ Services/TodoDbContext.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using TodoMvc.Models;
 
 namespace TodoMvc.Services
 {
-    public class TodoDbContext : DbContext, ITodoRepository
+    public class TodoDbContext : DbContext
     {
 
-        public DbSet<TodoItem> Todos { get; private set; }
+        public DbSet<TodoItem> Todos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -23,37 +18,5 @@ namespace TodoMvc.Services
             optionsBuilder.UseInMemoryDatabase("Todo");
         }
 
-        public async Task Add([Bind(nameof(TodoItem.Description))] TodoItem todo)
-        {
-            Todos.Add(todo);
-            await SaveChangesAsync();
-        }
-
-        public async Task<TodoItem> Find(Guid id)
-        {
-            return await Todos.FindAsync(id);
-        }
-
-        public async Task<IEnumerable<TodoItem>> GetAll()
-        {
-            return await Todos.ToListAsync();
-        }
-
-        public async Task Remove(Guid id)
-        {
-            var todo = await Todos.FindAsync(id);
-            Todos.Remove(todo);
-            await SaveChangesAsync();
-        }
-
-        public async Task Update(TodoItem todo)
-        {
-            TodoItem toUpdate = await Todos.FindAsync(todo.Id);
-            if (toUpdate != null)
-            {
-                toUpdate.Description = todo.Description;
-                await SaveChangesAsync();
-            }
-        }
     }
 }
---
+++ Services/TodoRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoMvc.Models;
+
+
+
+namespace TodoMvc.Services
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private readonly TodoDbContext _dbContext;
+
+        public TodoRepository(TodoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Add([Bind(nameof(TodoItem.Description))] TodoItem todo)
+        {
+            _dbContext.Todos.Add(todo);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<TodoItem> Find(Guid id)
+        {
+            return await _dbContext.Todos.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TodoItem>> GetAll()
+        {
+            return await _dbContext.Todos.ToListAsync();
+        }
+
+        public async Task Remove(Guid id)
+        {
+            var todo = await _dbContext.Todos.FindAsync(id);
+            _dbContext.Todos.Remove(todo);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(TodoItem todo)
+        {
+            TodoItem toUpdate = await _dbContext.Todos.FindAsync(todo.Id);
+            if (toUpdate != null)
+            {
+                toUpdate.Description = todo.Description;
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
